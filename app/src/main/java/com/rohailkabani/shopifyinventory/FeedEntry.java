e4eabaaed690011cc0da499ff90dfc1d6b352255
Rohail Kabani <rohail.kabani@gmail.com>
2018-01-03 10:30:06 -0500

    add class to temp store JSON entries

---
+++ app/src/main/java/com/rohailkabani/shopifyinventory/FeedEntry.java
@@ -0,0 +1,33 @@
+package com.rohailkabani.shopifyinventory;
+
+/**
+ * Created by rohailkabani on 2018-01-03.
+ */
+
+public class FeedEntry {
+    private String image, description, title;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
